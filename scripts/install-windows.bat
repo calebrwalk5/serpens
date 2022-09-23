35680b8c1e59c694a3876f0a2a3e6216d5a68e11
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-23 12:46:33 -0400

    Update install-windows.bat

--- scripts/install-windows.bat
+++ scripts/install-windows.bat
@@ -1,6 +1,7 @@
 @echo off
 color 1f
 echo Opening data model in browser
-start https://drive.google.com/file/d/1Goci9y1zpY47zF7k0DUtiM1KiD15SgXG/view
 echo save this to serpens/models/
+start https://drive.google.com/file/d/1Goci9y1zpY47zF7k0DUtiM1KiD15SgXG/view
+pip3 install -r ../requirements.txt
 pause
