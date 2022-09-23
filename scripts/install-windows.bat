7df4564dc7dc8fd9b7c928dbcb0782afd835a2d4
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-23 12:51:18 -0400

    Update install-windows.bat

--- scripts/install-windows.bat
+++ scripts/install-windows.bat
@@ -4,4 +4,5 @@ echo Opening data model in browser
 echo save this to serpens/models/
 start https://drive.google.com/file/d/1Goci9y1zpY47zF7k0DUtiM1KiD15SgXG/view
 pip3 install -r ../requirements.txt
+pip3 install scikit_learn
 pause
